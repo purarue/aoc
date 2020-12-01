048698730a90c9bea905edeff21537991f6337ee
Sean Breckenridge <seanbrecke@gmail.com>
2020-12-01 14:14:30 -0800

    fix confusing variable name

--- 001-python/001.py
+++ 001-python/001.py
@@ -4,7 +4,7 @@ with open(sys.argv[1]) as f:
     data = sorted(map(int, f.read().splitlines()))
 
 sdata = set(data)
-slen = len(data)
+llen = len(data)
 
 # a
 for x in data:
@@ -14,9 +14,9 @@ for x in data:
         break
 
 # b
-for i in range(slen):
+for i in range(llen):
     y = 2020 - data[i]
-    for j in range(slen - 1, i, -1):
+    for j in range(llen - 1, i, -1):
         target = y - data[j]
         if target in sdata:
             print(data[i] * data[j] * target)
