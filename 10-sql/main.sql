eef0b89cc9d2890c1aa148453a38aa2941964460
Sean Breckenridge <seanbrecke@gmail.com>
2020-12-09 23:21:18 -0800

    format SQL queries

--- 10-sql/main.sql
+++ 10-sql/main.sql
@@ -98,7 +98,9 @@ INSERT INTO Input (Val) VALUES (128);
 use aoc;
 
 CREATE OR REPLACE VIEW working AS
-  SELECT Val FROM Input
+SELECT Val
+FROM
+  Input
   UNION ALL SELECT 0
   UNION ALL (SELECT MAX(Val) + 3 FROM Input LIMIT 1);
 
@@ -110,10 +112,11 @@ WHERE Val + 1 IN (SELECT Val FROM working);
 CREATE OR REPLACE VIEW diff3 AS
 SELECT Val
 FROM working
-WHERE Val + 3 IN (SELECT Val FROM working)
-AND Val NOT IN (SELECT Val FROM diff1);
+WHERE
+  Val + 3 IN (SELECT Val FROM working)
+  AND Val NOT IN (SELECT Val FROM diff1);
 
 SELECT plus1.cnt * plus3.cnt as `Part 1`
 FROM
-(SELECT COUNT(*) `cnt` FROM diff1) as plus1,
-(SELECT COUNT(*) `cnt` FROM diff3) as plus3;
+  (SELECT COUNT(*) `cnt` FROM diff1) as plus1,
+  (SELECT COUNT(*) `cnt` FROM diff3) as plus3;
